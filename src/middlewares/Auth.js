d38509a554a58e60a0707cd1202d700b6ef9fed4
spandey6395 <spandey6395@gmail.com>
2022-05-10 13:40:32 +0530

    assign

---
+++ src/middlewares/Auth.js
@@ -0,0 +1,20 @@
+
+const jwt = require ("jsonwebtoken")
+
+const auth = async function(req,res,next){
+
+    try{
+        const token = req.header('x-api-key')
+        if(!token) return res.status(400).send({status:false , msg:'Please add token'})
+        const decodedToken = jwt.verify(token,"Group-6")
+        if(!decodedToken) return res.status(400).send({status:false , msg: 'Invalid token'})
+        const tokenExpire = decodedToken.exp
+        if(tokenExpire <= Date.now()/1000) return res.status(400).send({status:false , msg: 'token Expires'})
+        next();
+    }
+    catch(error){
+        console.log(error)
+        return res.status(500).send({status:false,msg:error.msg})
+    }
+
+}
