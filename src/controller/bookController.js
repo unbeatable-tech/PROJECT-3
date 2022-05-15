102a8c875f11241a4e0e463ed5a43396795c23cc
unbeatable-tech <aliyadav950@gmail.com>
2022-05-15 17:07:08 +0530

    All done

--- src/controller/bookController.js
+++ src/controller/bookController.js
@@ -98,63 +98,51 @@ const createBook = async function (req, res) {
 
 
 const getBooks = async function (req, res) {
+
   try {
-    let filters = { isDeleted: false };
-    let book = req.query.userId;
+    if (Object.keys(req.query).length == 0) {
+      let allBooks = await bookModel.find({ isDeleted: false }).select({ _id: 1, title: 1, excerpt: 1, userId: 1, category: 1, releasedAt: 1, reviews: 1 }).sort({ title: 1 })
+
+      if (allBooks.length == 0)
+        return res.status(404).send({ status: false, message: "No books exists" })
+      return res.status(200).send({ status: true, message: `Books List`, data: allBooks })
+    }
+    else{
 
 
-    if (book) {
-      if (!validator.isValidObjectId(book)) {
-        return res
-          .status(400)
-          .send({ status: false, message: "invalid UserId" });
+      let userId=req.query.userId
+      let category=req.query.category
+      let subcategory=req.query.subcategory
+
+      if(!validator.isValidObjectId(userId)){
+        return res.status(400).send({status:false,msg:"invalid userId"})
       }
-      filters["userId"] = book;
-    }
-    let cat = req.query.category;
-    if (cat) {
 
-      filters["category"] = cat;
-    }
-    let subcat = req.query.sucategory;
-    if (subcat) {
+      let allBooks = await bookModel.find({ $and: [{ $or: [{ userId: userId }, { category: category }, { subcategory: subcategory }] }, { isDeleted: false }] }).sort({ title: 1 })
+      .select({ _id: 1, title: 1, excerpt: 1, userId: 1, category: 1, releasedAt: 1, reviews: 1 })
 
-      filters["subcategory"] = subcat;
-    }
-    let bookData = await bookModel.find(filters).select({
-      _id: 1,
-      title: 1,
-      excerpt: 1,
-      userId: 1,
-      category: 1,
-      reviews: 1,
-      releasedAt: 1,
-    });
+      if (allBooks.length == 0)
+      return res.status(400).send({ status: false, message: "No books with selected query params" })
 
-    const bookDetails = bookData.sort(function (a, b) {
-      if (a.title.toLowerCase() < b.title.toLowerCase()) {
-        return -1;
-      }
-      if (a.title.toLowerCase() > b.title.toLowerCase()) {
-        return 1;
-      }
-      return 0;
-    });
-    if (bookData.length > 0) {
-      return res.status(200).send({
-        status: true,
-        count: bookDetails.length,
-        message: "Book List",
-        data: bookDetails,
-      });
-    } else {
-      return res.status(404).send({ status: false, msg: "No books found" });
+      
+      res.status(200).send({ status: true, message: "Books List", data: allBooks })
     }
-  } catch (error) {
-    console.log(error);
-    return res.status(500).send({ status: false, msg: error.message });
+
+
+
+    
+
+
+
+
+
   }
-};
+  catch (error) {
+    console.log(error)
+    return res.status(500).send({ status: false, msg: error.message })
+  }
+
+}
 
 
 
@@ -162,16 +150,17 @@ const getBooks = async function (req, res) {
 const getbookbyId = async function (req, res) {
   try {
     let Id = req.params.bookId;
-    
+
     if (!validator.isValidObjectId(Id)) {
       return res
         .status(400)
         .send({ status: false, msg: "pls provide valid bookId" });
     }
 
-    let findbook = await bookModel.findOne({ _id: Id, isDeleted: false });
-    if (findbook) {
-      let { title, excerpt, userId, category, subcategory, isDeleted, reviews, deletedAt, releasedAt, createdAt, updatedAt } = findbook
+    let findbook = await bookModel.findOne({ _id: Id, isDeleted: false }).select({__v:0,ISBN:0});
+    if (!findbook) {
+      return res.status(404).send({ status: false, msg: "book not present " })
+    }
 
       const reviewsData = await reviewModel.find({ bookId: Id, isDeleted: false }).select({ createdAt: 0, updatedAt: 0, __v: 0 })
       const data = { title, excerpt, userId, category, subcategory, isDeleted, reviews, deletedAt, releasedAt, createdAt, updatedAt, reviewsData }
@@ -180,10 +169,9 @@ const getbookbyId = async function (req, res) {
       return res.status(200).send({ status: true, messege: "Book List", Data: data })
 
     }
-    else{
-      return res.status(400).send({status:false,msg:"book already deleted"})
-    }
-  } catch (error) {
+    
+  
+  catch (error) {
     console.log(error);
     return res.status(500).send({ status: false, msg: error.message });
   }
